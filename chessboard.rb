97f4294c034685cedd15c3fe2af77300e50127d6
Andreas Bach Landgrebe <andreaslandgrebe@gmail.com>
2017-08-31 10:22:42 -0400

    finished release 0 by generating a chessboard in an nested array to pass all of the written rspec tests

---
+++ chessboard.rb
@@ -0,0 +1,18 @@
+
+
+def generate_chessboard
+
+ board = [
+  ['B Rook', 'B Knight', 'B Bishop', 'B Queen', 'B King', 'B Bishop', 'B Knight', 'B Rook'],
+  ['B Pawn', 'B Pawn', 'B Pawn', 'B Pawn', 'B Pawn', 'B Pawn', 'B Pawn', 'B Pawn'],
+  [nil, nil, nil, nil, nil, nil, nil, nil],
+  [nil, nil, nil, nil, nil, nil, nil, nil],
+  [nil, nil, nil, nil, nil, nil, nil, nil],
+  [nil, nil, nil, nil, nil, nil, nil, nil],
+  ['W Pawn', 'W Pawn', 'W Pawn', 'W Pawn', 'W Pawn', 'W Pawn', 'W Pawn', 'W Pawn', ],
+  ['W Rook', 'W Knight', 'W Bishop', 'W Queen', 'W King', 'W Bishop', 'W Knight', 'W Rook']
+]
+
+end
+
+
