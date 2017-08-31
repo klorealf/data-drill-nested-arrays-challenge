520b0ea2a7002ed45a87999830c5a44877d565bf
Catherine Rice <clr2107@gmail.com>
2017-08-31 09:58:22 -0400

    Update chessboard

--- chessboard.rb
+++ chessboard.rb
@@ -1,18 +1,17 @@
 def generate_chessboard
 chessboard = [
-  ['B Castle', 'B Knight', 'B Bishop', 'B Queen', 'B King', 'B Bishop', 'B Knight', 'B Castle'],
+  ['B Rook', 'B Knight', 'B Bishop', 'B Queen', 'B King', 'B Bishop', 'B Knight', 'B Rook'],
   ['B Pawn', 'B Pawn', 'B Pawn', 'B Pawn', 'B Pawn', 'B Pawn', 'B Pawn', 'B Pawn'],
   [nil, nil, nil, nil, nil, nil, nil, nil],
   [nil, nil, nil, nil, nil, nil, nil, nil],
   [nil, nil, nil, nil, nil, nil, nil, nil],
   [nil, nil, nil, nil, nil, nil, nil, nil],
-  [nil, nil, nil, nil, nil, nil, nil, nil],
   ['W Pawn', 'W Pawn', 'W Pawn', 'W Pawn', 'W Pawn', 'W Pawn', 'W Pawn', 'W Pawn'],
-  ['W Castle', 'W Knight', 'W Bishop', 'W Queen', 'W King', 'W Bishop', 'W Knight', 'W Castle']
+  ['W Rook', 'W Knight', 'W Bishop', 'W Queen', 'W King', 'W Bishop', 'W Knight', 'W Rook']
 ]
-p chessboard
+# p chessboard
 # p first_row = chessboard[0]
-p chessboard[7][0]
+# p chessboard[7][0]
 end
-generate_chessboard
+# generate_chessboard
 
